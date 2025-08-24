2a830bbb599522be3ce8a8798d4fbedc32b8c536
notlelouch <notlelouchvb@gmail.com>
2025-08-25 00:23:14 +0530

    only backend

--- frontend/src/components/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-export const ErrorMessage = ({ error }) => {
-    if (!error) return null;
-  
-    return (
-      <div className="bg-red-100 border-l-4 border-red-500 p-6 mb-8 rounded-r-xl shadow-lg">
-        <p className="text-red-700 font-medium">{error}</p>
-      </div>
-    );
-  };
