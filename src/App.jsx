664eaa1f70d1fd88d2f34db9cb88649bbe8811da
notlelouch <notlelouchvb@gmail.com>
2025-08-25 01:44:15 +0530

    replaced the local deployment link in the frontend with actual deployed link

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { PlantsGrid } from './components/PlantsGrid';
 import { AuthForm } from './components/AuthForm';
 
 // API service
-const API_BASE = 'http://localhost:3000/api';
+const API_BASE = 'https://urvann-nursery.onrender.com';
 
 const api = {
   getPlants: async () => {
--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -14,7 +14,7 @@ export const AuthForm = ({ onAuthSuccess, isAdmin }) => {
 
     try {
       const endpoint = isLogin ? '/api/login' : '/api/signup';
-      const response = await fetch(`http://localhost:3000${endpoint}`, {
+      const response = await fetch(`https://urvann-nursery.onrender.com${endpoint}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
